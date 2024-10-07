9914052c7ab2f05260e1f530ed76421216e3550b
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-10-07 19:02:50 +0530

    Update Navbar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter=Inter({
-  subsets: ['latin'],
-})
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,10 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={inter.className}
-      >
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <div className="relative w-full flex items-center justify-center ">
+          <Navbar />
+        </div>
         {children}
       </body>
     </html>
