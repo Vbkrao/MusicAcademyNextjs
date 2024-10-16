ddb7ab160c2cd2c5a1f550caa466efa6756ee70c
Bala <82591411+Vbkrao@users.noreply.github.com>
2024-10-16 22:59:39 +0530

    Update Featured Courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/HeroSection"
-import FeaturedCourses from "@/components/ui/FeaturedCourses";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
